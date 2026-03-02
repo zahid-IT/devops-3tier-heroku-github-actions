aa2518b4600033209355c5ea222df9621734d46e
zahid-IT <zahidbilal2k15@gmail.com>
2026-03-02 14:33:32 +0500

    Change database host to 'postgres_db'

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "postgres_db",
   database: process.env.DB_NAME || "mydb",
   password: process.env.DB_PASSWORD || "postgres",
   port: process.env.DB_PORT || 5432,
